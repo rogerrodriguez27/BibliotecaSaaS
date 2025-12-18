fb07101767cd60363eef37005a43d01f1b59b533
rogerrodriguez27 <roger.rodriguez.27@unsch.edu.pe>
2025-12-17 21:39:22 -0500

    cambio en Program.cs

--- Biblioteca.API/Program.cs
+++ Biblioteca.API/Program.cs
@@ -1,5 +1,6 @@
 using Biblioteca.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,8 +12,12 @@ builder.Services.AddDbContext<ApplicationDbContext>(options =>
 // ====================================================
 
 // Add services to the container.
-
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        // Esta línea rompe los bucles infinitos automáticamente
+        options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    });
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
